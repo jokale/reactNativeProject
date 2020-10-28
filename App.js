6844bd2947d463318c325756f775736cecb1f709
jokale <kalemajoanna@gmail.com>
2020-10-28 15:16:09 +0000

    create a list and scroll view

--- App.js
+++ App.js
@@ -1,42 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const[name, setName] = useState('Joanna')
-  const[age, setAge] = useState(22)
+  const[people, setPeople] = useState([
+    {name: 'joanna', key: '1'},
+    {name: 'jo', key: '2'},
+    {name: 'joanne', key: '3'},
+    {name: 'joann', key: '4'},
+    {name: 'johannah', key: '5'}
 
-  // using setName or set whatever is just naming convention 
-  //we can pass in any type of object into the state a boolean, string array
 
+  ])
 
-  // good to put the logic above because placing it in the OnPress function would make it messy 
-  // it seems I can put all my update state in one click 
-  
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>What is your name?</Text>        
-       <Text> My name is {name} </Text>
-  
-       <TextInput
-       multiline
-        style={styles.input}
-        placeholder='e.g My name is Jane Doe'
-                onChangeText={(value)=> setName(value)}
-        />
-      <Text>What is your age?</Text>    
-      <Text>I am {age} years old </Text>
-   
-       <TextInput
-       keyboardType='numeric'
-       multiline
-        style={styles.input}
-        placeholder='e.g 100'
-                onChangeText={(value)=> setAge(value)}
-        />
+      {people.map(item => {
+        return (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )
+
+      })}
 
         
-    </View>
+    </View></ScrollView>
   );
 }
 
@@ -45,14 +35,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item:{
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'purple',
+    fontSize: 24,
   },
+
 });
