07290ed2bef416d2c58c2f072e17a00b76b6897e
jokale <kalemajoanna@gmail.com>
2020-10-28 12:51:19 +0000

    add text input

--- App.js
+++ App.js
@@ -1,55 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const[name, setName] = useState('Joanna')
+  const[age, setAge] = useState(22)
+
   // using setName or set whatever is just naming convention 
   //we can pass in any type of object into the state a boolean, string array
 
-  const[person, setPerson] = useState({ name: 'Jonathan', age: 23})
 
   // good to put the logic above because placing it in the OnPress function would make it messy 
   // it seems I can put all my update state in one click 
   
-  const pressHandler =()=> {
-    setName('Jo Jo Joanna')
-    setPerson({name: 'Jo Jo Jonathan', age: 24})
-  }
   return (
     <View style={styles.container}>
-      <View style={styles.header}> 
-      <Text style={styles.boldText}>Yo</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText} >My name is {name}</Text>
-        <Text>My brothers name is {person.name} and he is {person.age} years old</Text>
-        <View style={styles.buttonContainer}>
-        <Button title='updating state' onPress={pressHandler}/>
-        </View>
-      </View>
+      <Text> My name is {name} and I am {age} years old </Text>
+      <Text>What is your name?</Text>     
+       <TextInput/>
+
     </View>
   );
 }
 
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F1B9E6',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    padding: 60,
-    backgroundColor: 'purple',
-  }
 });
